01c60aa84b1f3119546cd593bf2c32ac8ce042ea
cmenegusWHAT <clairemenegus@gmail.com>
2016-02-01 20:42:36 -0500

    class activity variable

--- Solving Sol Wall Drawing  #328/sketch.js
+++ Solving Sol Wall Drawing  #328/sketch.js
@@ -1,17 +1,29 @@
+var circle1;
+var circle2;
+var circle3;
+var circle4;
+var circle5;
+
 function setup() {
 createCanvas(800,500);
+circle1=400;
+circle2=300;
+circle3=250;
+circle4=200;
+circle5=0;
+
 }
 function draw() {
-  background(0,0,0);
+  background(circle5,circle5,circle5);
   var c= color(0,0,0)
   fill(c)
-  ellipse(400,250,400,400);
-  stroke(250,250,250);
+  ellipse(circle1,circle3,circle1,circle1);
+  stroke(circle3,circle3,circle3);
   line (450,58,450,443);
   line (350,58,350,443);
-  line(400,50,400,450);
-  quad(375, 200, 500, 200, 425, 300, 300, 300);
-  line(340,250,460,250);
+  line(circle1,50,circle1,450);
+  quad(375, circle4, 500, circle4, 425, circle2, circle2, circle2);
+  line(340,250,460,circle3);
   line(346,240,470,240);
   line(332,260,452,260);
     
--- Solving Sol Wall Drawing #17/sketch.js
+++ Solving Sol Wall Drawing #17/sketch.js
@@ -1,35 +1,46 @@
+var burrito1
+var burrito2
+var burrito3
+var burrito4
+var burrito5
+
 function setup() {
   createCanvas(800,500);
+burrito1=1;
+burrito2=35;
+burrito3=40
+burrito5=80
+burrito6=125
 }
 function draw() {
   background(255);
-  line(1,1,1,35);
-  line(6,1,6,35);
-  line(10,1,10,35);
-  line(15,1,15,35);
-  line (20,1,20,35); 
+  line(burrito1,burrito1,burrito1,burrito2);
+  line(6,burrito1,6,burrito2);
+  line(10,burrito1,10,burrito2);
+  line(15,burrito1,15,burrito2);
+  line (20,burrito1,20,burrito2); 
   //secondrectangle//
-  line(40,1,60,1);
-  line(40,5,60,5);
-  line(40,10,60,10);
-  line(40,15,60,15);
-  line(40,20,60,20);
-  line(40,25,60,25);
-  line(40,30,60,30);
-  line(40,35,60,35); 
+  line(burrito3,burrito1,60,burrito1);
+  line(burrito3,5,60,5);
+  line(burrito3,10,60,10);
+  line(burrito3,15,60,15);
+  line(burrito3,20,60,20);
+  line(burrito3,25,60,25);
+  line(burrito3,30,60,30);
+  line(burrito3,burrito2,60,burrito2); 
   //third rectangle//
-  line(80,5,85,0);
-  line(80,10,90,0);
-  line(80,15,95,0);
-  line(80,20,100,0);
-  line(80,25,105,0);
-  line(80,30,105,5);
-  line(80,35,105,10);
-  line(80,40,105,15);
-  line(85,40,105,20);
-  line(90,40,105,25);
-  line(95,40,105,30);
-  line(100,40,105,35);
+  line(burrito5,5,85,0);
+  line(burrito5,10,90,0);
+  line(burrito5,15,95,0);
+  line(burrito5,20,100,0);
+  line(burrito5,25,105,0);
+  line(burrito5,30,105,5);
+  line(burrito5,burrito2,105,10);
+  line(burrito5,burrito3,105,15);
+  line(85,burrito3,105,20);
+  line(90,burrito3,105,25);
+  line(95,burrito3,105,30);
+  line(100,burrito3,105,35);
   //fourth rectangle//
   line(145,0,150,5)
   line(140,0,150,10)
@@ -38,11 +49,11 @@ function draw() {
   line(125,0,150,25)
   line(125,5,150,30)
   line(125,10,150,35)
-  line(125,15,150,40)
-  line(125,20,145,40)
-  line(125,25,140,40)
-  line(125,30,135,40)
-  line(125,35,130,40)
+  line(125,15,150,burrito3)
+  line(125,20,145,burrito3)
+  line(125,25,140,burrito3)
+  line(125,30,135,burrito3)
+  line(125,burrito2,130,burrito3)
 
  
   
